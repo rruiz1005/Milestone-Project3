fa95727e33c16108e0d6f3d8acebe50fad7d0f22
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-09-30 22:37:18 -0400

    User Profile setup

--- shareme_frontend/src/components/UserProfile.jsx
+++ shareme_frontend/src/components/UserProfile.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from '@react-oauth/google';
+
+import { userCreatedPinsQuery, userQuery, userSavedPinsQuery } from '../utils/data';
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
 
 const UserProfile = () => {
+    const [user, setUser] = useState();
+    const [pins, setPins] = useState();
+    const [text, setText] = useState('Created');
+    const [activeBtn, setActiveBtn] = useState('created');
+    const navigate = useNavigate();
+    const { userId } = useParams();
     return (
         <div>
             UserProfile
