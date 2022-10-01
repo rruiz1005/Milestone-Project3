3fd7e3528a5c1d32c88617152957fb87ec5d6852
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-10-01 03:49:04 -0400

    generate site output folder

---
+++ Pikucha_frontend/src/index.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Router>
+        <App />
+    </Router>,
+    // document.getElementById('root')
+);
